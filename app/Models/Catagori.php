40dc5d9bf8af1334a524f6d133f4c1fbe5914a30
MUHAMMAD_MUHAMMAD <laniwambuliga@gmail.com>
2024-02-04 07:17:36 +0700

    update pembelajaran

--- app/Models/Catagori.php
+++ app/Models/Catagori.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Catagori extends Model
 {
     use HasFactory;
+    protected $table = 'catagories';
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,32 +2,33 @@
 
 namespace Database\Seeders;
 
-use App\Models\Catagori;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Schema;
 
-class CatagoriesSeeder extends Seeder
+class RoleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        
         Schema::disableForeignKeyConstraints();
-        Catagori::truncate();
+        Role::truncate();
         Schema::enableForeignKeyConstraints();
 
         $data = [
-            "Horor", "novel", "fantasy", "fiction", "mystery", "romace"
+            "admin", "client"
         ];
 
        foreach ($data as   $value) {
-        Catagori::insert([
+        Role::insert([
             "name" => $value
         ]);
        }      
            
+        
     }
 }
